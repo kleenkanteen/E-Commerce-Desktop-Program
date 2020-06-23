fcd677e87bd5effbf69dc3edc5ff0f50baf0ad84
Jing Jing Yuan <angel.yuan@mail.utoronto.ca>
2020-06-23 17:01:16 -0400

    Creating the source root Creating the abstract class AccountInformation

---
+++ phase1/src/AccountInformation.java
@@ -0,0 +1,11 @@
+public abstract class AccountInformation {
+    private String username, password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
