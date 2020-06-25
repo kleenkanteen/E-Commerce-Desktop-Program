11df436a428dff5dba6add04d04b52dea3298b97
Jing Jing Yuan <angel.yuan@mail.utoronto.ca>
2020-06-24 21:26:21 -0400

    Creating a constructor for the abstract class AccountInformation

--- phase1/src/AccountInformation.java
+++ phase1/src/AccountInformation.java
@@ -1,6 +1,12 @@
 public abstract class AccountInformation {
     private String username, password;
 
+
+    public AccountInformation(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
     public String getUsername() {
         return username;
     }
