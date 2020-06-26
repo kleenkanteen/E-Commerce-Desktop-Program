dd3e4ec58a7aa98357bd026d04bf001bb1a8965e
Jing Jing Yuan <angel.yuan@mail.utoronto.ca>
2020-06-26 07:15:57 -0400

    Created admin

---
+++ phase1/src/Admin.java
@@ -0,0 +1,11 @@
+public class Admin extends AccountInformation{
+    /**
+     * Creates an admin with the given username and password
+     *
+     * @param username is the username of this account
+     * @param password is the password of this account
+     */
+    protected Admin(String username, String password) {
+        super(username, password);
+    }
+}
