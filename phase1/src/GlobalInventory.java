274673fdb79a49732ffce74fce2f6ae1a33580b5
QIyi Zhang <qiyi.zhang@mai.utoronto.ca>
2020-06-26 11:17:11 -0400

    Item Class

--- phase1/src/GlobalInventory.java
+++ phase1/src/GlobalInventory.java
@@ -2,6 +2,9 @@ import java.util.ArrayList;
 
 public class GlobalInventory {
     ArrayList<Item> itemList;
+
+    //use default constructor in main method
+
     public void addItem(Item item){
         itemList.add(item);
     }
@@ -11,10 +14,25 @@ public class GlobalInventory {
     public boolean contains(Item item){
         return itemList.contains(item);
     }
+    public ArrayList<Item> searchByOwner(String owner){
+        ArrayList<Item> banana = new ArrayList<Item>(); //😀
+        for (Item item : itemList){
+            if (item.ownerName.equals(owner)){
+                banana.add(item);
+            }
+        }
+        return banana;
+    }
+    public ArrayList<Item> searchByItemName(String itemName){
+        ArrayList<Item> banana = new ArrayList<Item>(); //😂
+        for (Item item : itemList){
+            if (item.name.equals(itemName)){
+                banana.add(item);
+            }
+        }
+        return banana;
 
 
 
-
-
-
+}
 }
