6479f4a48b49ffb88b3942377a8180c0187a2c92
coolmanbat2 <thanusun.thayaparan@mail.utoronto.ca>
2020-06-25 22:23:19 -0400

    Added a Trade class with all the specifications satisfied from CRC

---
+++ phase1/src/Trade.java
@@ -0,0 +1,52 @@
+public abstract class Trade {
+    private final User seller;
+    private final User buyer;
+    private final Item itemToTrade;
+    private final String time;
+    private final String date;
+
+    /**
+     * Creates a trade with an item that both the seller wants to sell
+     * and the buyer wants to buy.
+     * The buyer creates the trade with the item that they want to sell.
+     * @param itemToTrade takes in an item that both the buyer and seller want.
+     * @param date is a string that follows a specific date format.
+     * @param time is a string tha follows a time.
+     */
+    public Trade(Item itemToTrade, String date, String time) {
+        this.seller = new User();
+        this.buyer = new User();
+        this.date = date;
+        this.time = time;
+        this.itemToTrade = itemToTrade;
+    }
+
+    /**
+     * Returns a boolean that determines whether the trade has been completed.
+     * If not, the method returns false.
+     *
+     * Note: Useful for admins, especially if they want to freeze accounts!
+     * @param sellerAccepts takes a boolean and tells us that the seller accepts all details
+     * @param buyerAccepts takes a boolean and tells us that the buyer accepts all the details.
+     * @return
+     */
+    public boolean tradeCompleted(boolean sellerAccepts, boolean buyerAccepts) {
+        boolean completed = false;
+        if (sellerAccepts == buyerAccepts) {
+            completed = true;
+        }
+        return completed;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public Item getItemToTrade() {
+        return itemToTrade;
+    }
+}
