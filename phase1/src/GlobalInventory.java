7b8c066c8eea85cc238c0b9c9a4524328f4fe84c
QIyi Zhang <qiyi.zhang@mai.utoronto.ca>
2020-06-26 09:51:04 -0400

    Item Class

--- phase1/src/GlobalInventory.java
+++ phase1/src/GlobalInventory.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+
 public class GlobalInventory {
+    ArrayList<Item> itemList;
+    public void addItem(Item item){
+        itemList.add(item);
+    }
+    public void removeItem(Item item){
+        itemList.remove(item);
+    }
+    //public void returnTenItems(){} I am thinking if we should put it into UserManager
+
+
+
 
 }
