e8351d1143e54c0adededbe94c8dd472697c7e3d
Oliver Li <oliverli.li@mail.utoronto.ca>
2020-06-29 18:57:50 -0400

    DEFINITELY unscrewed UserManager.java

--- phase1/src/UserManager.java
+++ phase1/src/UserManager.java
@@ -3,18 +3,40 @@ import java.lang.System;
 import java.util.Scanner;
 import java.util.HashMap;
 import java.io.*;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class UserManager implements Serializable{
-    private Hashmap<String, User> allUsers;
+    private static final Logger logger = Logger.getLogger(AdminAccountManager.class.getName());
+    private static final Handler consoleHandler = new ConsoleHandler();
+    private HashMap<String, User> allUsers;
 
     /**
-     * Creates a UserManager object. Final implementation will vary later (how to serialize? deserialize?)
+     * Creates a UserManager object.
+     * @param filepath Filepath to the .ser file storing the User objects
      */
-    public UserManager() {
-        this.allUsers = new Hashmap<String, User>();
+    public UserManager(String filepath) {
+        // create new file and check to see if file already exists
+        logger.setLevel(Level.ALL);
+        consoleHandler.setLevel(Level.ALL);
+        logger.addHandler(consoleHandler);
+        try {
+            File file = new File(filepath);
+            if (file.exists()) {
+                readFromFile(filepath);
+            } else {
+                file.createNewFile();
+            }
+        }
+        catch(IOException ex) {
+            System.out.println("Input error!");
+        }
     }
 
     /**
+     * Necessary?
      * Deserializes the arraylist of user objects into the program.
      * @param filepath Filepath to the .ser file storing the User objects.
      */
@@ -25,12 +47,17 @@ public class UserManager implements Serializable{
             InputStream buffer = new BufferedInputStream(file);
             ObjectInput input = new ObjectInputStream(buffer);
 
-            // deserialize the arraylist of user objects
-            allUsers = (Hashmap<String, User>) input.readObject();
+            // deserialize the hashmap of user objects
+            allUsers = (HashMap<String, User>) input.readObject();
             input.close();
         }
         catch(IOException ex) {
-            System.out.println("Input error during deserialization!");
+            logger.log(Level.SEVERE, "Cannot read from input during deserialization.", ex);
+            // System.out.println("Input error during deserialization!");
+        }
+        catch(ClassNotFoundException ex) {
+            logger.log(Level.SEVERE, "Class not found exception.", ex);
+            // System.out.println("Class not found exception!");
         }
     }
 
@@ -39,19 +66,21 @@ public class UserManager implements Serializable{
      * @param filepath where this file will be stored
      */
     public void writeToFile(String filepath) {
-    }
         try {
+        // load off objects...is that even the right term?
         FileOutputStream file = new FileOutputStream(filepath);
         OutputStream buffer = new BufferedOutputStream(file);
         ObjectOutputStream output = new ObjectOutputStream(buffer);
 
+        // serialize objects
         output.writeObject(allUsers);
         output.close();
-    }
+        }
         catch(IOException ex) {
-        System.out.println("Input error during serialization!");
+        logger.log(Level.SEVERE, "Cannot read from input during serialization.", ex);
+        // System.out.println("Input error during serialization!");
+        }
     }
-}
 
     /**
      * Takes in username, password, finds the right user name and returns it.
@@ -66,11 +95,13 @@ public class UserManager implements Serializable{
         // check username
         if(this.allUsers.containsKey(username)) {
             // check password
-            if(password == this.allUsers.get(username).getPassword()) {
+            if(password.equals(this.allUsers.get(username).getPassword())) {
+                logger.log(Level.INFO, "successful login, user object returned");
                 return this.allUsers.get(username);
             }
         }
-        throw new InvalidLoginException;
+        logger.log(Level.INFO, "Invalid login exception.");
+        throw new InvalidLoginException();
     }
 
     /**
@@ -82,13 +113,12 @@ public class UserManager implements Serializable{
      * @throws InvalidUsernameException username is already taken
      */
     public void createNewUser(String userName, String password) throws InvalidUsernameException{
-        /*
-        for(User user : this.allUsers) {
-            //check to see if this username already exists, if yes then:
-            throw new InvalidUsernameException;
+        if(!this.allUsers.containsKey(userName)) {
+            logger.log(Level.INFO, "Invalid username exception");
+            throw new InvalidUsernameException();
         }
-        this.allUsers.add(newUser);
-        */
+        logger.log(Level.INFO, "Successful account creation.");
+        this.allUsers.put(userName, new User(userName, password));
     }
 
     /**
@@ -99,13 +129,11 @@ public class UserManager implements Serializable{
      * @param userName String of username
      */
     public boolean removeUser(String userName) {
-        /*
-        if(get index of the user object with the username) {
-            this.allUsers.remove(this.allUsers.index(user));
+        if(this.allUsers.containsKey(userName)) {
+            this.allUsers.remove(userName);
             return true;
         }
         return false;
-        */
     }
 
     /**
@@ -113,7 +141,9 @@ public class UserManager implements Serializable{
      * @param user String username
      * @return Arraylist of user's items
      */
-    // public ArrayList<Item> getUserInventory (String user) { }
+    public ArrayList<Item> getUserInventory (String user) {
+        return this.allUsers.get(user).getPersonalInventory();
+    }
 
     /**
      * ARGUMENTS ARE NOT FINAL, JUST PLACEHOLDERS.
@@ -133,12 +163,7 @@ public class UserManager implements Serializable{
      * @param userName the User object in question.
      */
     public void freezeUserAccount(String userName) {
-        User selectedUser; //get index of user object with the username
-        if (selectedUser.isFrozen()) {
-            selectedUser.setFrozen(false);
-        }
-        else {
-            selecteduser.setFrozen(true);
-        }
+        User selectedUser = this.allUsers.get(userName);
+        selectedUser.setFrozen(!selectedUser.isFrozen());
     }
 }
