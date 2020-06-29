6e14b88efe937f34f23095f849abf082eb5c5fc0
coolmanbat2 <thanusun.thayaparan@mail.utoronto.ca>
2020-06-29 17:55:55 -0400

    Made changes to Trade and added PermTrade.

--- phase1/src/PermTrade.java
+++ phase1/src/PermTrade.java
@@ -1,19 +1,46 @@
 // Written by Thanusun
 
+import java.util.ArrayList;
+
 public class PermTrade extends Trade {
+
+    private void addItems(User trader, ArrayList<Item> itemsToTrade) {
+        for (Item tradeItem : itemsToTrade) {
+            trader.getPersonalInventory().add(tradeItem);
+            tradeItem.setOwnerName(trader.getUsername());
+        }
+    }
 
     /**
      * Creates a trade with an item that both the seller wants to sell
      * and the buyer wants to buy.
      * The buyer creates the trade with the item that they want to sell.
      *
-     * @param seller      takes in a User that wants to create the Trade.
-     * @param buyer       takes in a User that wants to borrow the item based on the trade.
-     * @param itemToTrade takes in an item that both the buyer and seller want.
+     * NOTE: <i>userAItemsToTrade</i> and <i>userBItemsToTrade</i> must not be empty at the same time.
+     * It will lead to very bad things.
+     * @param traderA     takes in a User that wants to create the Trade.
+     * @param traderB     takes in a User that wants to borrow the item based on the trade.
+     * @param userAItemsToTrade takes in items that want to be traded to userB.
+     * @param userBItemsToTrade takes in items that want to be traded to userA.
      * @param date        is a string that follows a specific date format.
      * @param time        is a string that follows a time.
      */
-    public PermTrade(User seller, User buyer, Item itemToTrade, String date, String time) {
-        super(seller, buyer, itemToTrade, date, time);
+    public PermTrade(User traderA, User traderB, ArrayList<Item> userAItemsToTrade, ArrayList<Item> userBItemsToTrade, String date, String time) {
+        super(traderA, traderB, userAItemsToTrade, userBItemsToTrade, date, time);
+
+        // two-way trade.
+        if (!(userAItemsToTrade.isEmpty() && userBItemsToTrade.isEmpty())) {
+            // for every item add it into personalInventory. (traderA)
+            addItems(traderB, userAItemsToTrade);
+            // traderB items'
+            addItems(traderA, userBItemsToTrade);
+        // one-way trade.
+        } else {
+            if (userAItemsToTrade.isEmpty()) {
+                addItems(traderA, userBItemsToTrade);
+            } else {
+                addItems(traderB, userBItemsToTrade);
+            }
+        }
     }
 }
