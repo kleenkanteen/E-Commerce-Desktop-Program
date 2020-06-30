8186818d097c456d35ba20e1ce29b5608d78a0b5
Jing Jing Yuan <angel.yuan@mail.utoronto.ca>
2020-06-30 17:47:40 -0400

    Change messages subclasses

--- phase1/src/entities/FreezeRequestMessage.java
+++ phase1/src/entities/FreezeRequestMessage.java
@@ -1,15 +1,15 @@
 package entities;
 
 public class FreezeRequestMessage extends DecisionMessage {
-    private static final String[] options = new String[]{"freeze", "not freeze"};
     private User user;
 
     /**
      * A message sent to a entities.User that request in making a change to
      * @param content is the content of the message
      */
-    public FreezeRequestMessage(String content) {
-        super(content, options);
+    public FreezeRequestMessage(String content, User user) {
+        super(content, new String[]{"freeze", "not freeze"});
+        this.user = user;
     }
 
     /**
