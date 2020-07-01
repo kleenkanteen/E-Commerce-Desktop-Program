1e342ea66c50007d404e778e37877832c2ee7de1
coolmanbat2 <thanusun.thayaparan@mail.utoronto.ca>
2020-07-01 06:45:02 -0400

    Added TempTrade.daysLeft method in TempTrade, along with functionality of TempTrade.
    Also, removed "Calendar time" from TempTrade, PermTrade, and Trade.

--- phase1/src/entities/PermTrade.java
+++ phase1/src/entities/PermTrade.java
@@ -1,13 +1,20 @@
 package entities;// Written by Thanusun
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class PermTrade extends Trade {
 
-    private void addItems(User trader, ArrayList<Item> itemsToTrade) {
+    private boolean borrowed, lent = false;
+
+
+    protected void addItems(User trader, ArrayList<Item> itemsToTrade) {
+        // for every item in the users' items to trade.
         for (Item tradeItem : itemsToTrade) {
+            // add it to their inventory.
             trader.getPersonalInventory().add(tradeItem);
             tradeItem.setOwnerName(trader.getUsername());
+            itemsToTrade.remove(tradeItem);
         }
     }
 
@@ -18,15 +25,14 @@ public class PermTrade extends Trade {
      *
      * NOTE: <i>userAItemsToTrade</i> and <i>userBItemsToTrade</i> must not be empty at the same time.
      * It will lead to very bad things.
-     * @param traderA     takes in a entities.User that wants to create the entities.Trade.
-     * @param traderB     takes in a entities.User that wants to borrow the item based on the trade.
+     * @param traderA takes in a entities.User that wants to create the entities.Trade.
+     * @param traderB takes in a entities.User that wants to borrow the item based on the trade.
      * @param userAItemsToTrade takes in items that want to be traded to userB.
      * @param userBItemsToTrade takes in items that want to be traded to userA.
-     * @param date        is a string that follows a specific date format.
-     * @param time        is a string that follows a time.
+     * @param date is a string that follows a specific date format.
      */
-    public PermTrade(User traderA, User traderB, ArrayList<Item> userAItemsToTrade, ArrayList<Item> userBItemsToTrade, String date, String time) {
-        super(traderA, traderB, userAItemsToTrade, userBItemsToTrade, date, time);
+    public PermTrade(User traderA, User traderB, ArrayList<Item> userAItemsToTrade, ArrayList<Item> userBItemsToTrade, Calendar date) {
+        super(traderA, traderB, userAItemsToTrade, userBItemsToTrade, date);
 
         // two-way trade.
         if (!(userAItemsToTrade.isEmpty() && userBItemsToTrade.isEmpty())) {
@@ -38,6 +44,7 @@ public class PermTrade extends Trade {
         } else {
             if (userAItemsToTrade.isEmpty()) {
                 addItems(traderA, userBItemsToTrade);
+
             } else {
                 addItems(traderB, userBItemsToTrade);
             }
