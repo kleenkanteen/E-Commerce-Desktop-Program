aca65dae7e738e2ad628e64cde4f73a19eb27756
Jing Jing Yuan <angel.yuan@mail.utoronto.ca>
2020-07-02 19:27:10 -0400

    Changes to message

--- phase1/src/entities/NewItemMessage.java
+++ phase1/src/entities/NewItemMessage.java
@@ -7,8 +7,8 @@ public class NewItemMessage extends DecisionMessage {
      * A message sent to a User that request in making a change to
      * @param content is the content of the message
      */
-    public NewItemMessage(String content, Item item, String username) {
-        super(content, new String[]{"confirm", "deny"}, username);
+    public NewItemMessage(String content, Item item) {
+        super(content, new String[]{"confirm", "deny"}, item.getOwnerName());
         this.newItem = item;
     }
 
