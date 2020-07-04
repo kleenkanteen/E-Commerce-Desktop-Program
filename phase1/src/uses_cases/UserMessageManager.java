afab2760f10c361995a0b455a6af1604c464b76d
Oliver Li <oliverli.li@mail.utoronto.ca>
2020-07-03 22:35:08 -0400

    implemented managing trade requests in MessageManager (still needs testing)

--- phase1/src/uses_cases/UserMessageManager.java
+++ phase1/src/uses_cases/UserMessageManager.java
@@ -5,6 +5,8 @@ import entities.TradeRequestMessage;
 import entities.TradeRequest;
 import entities.PermTrade;
 import entities.TempTrade;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -60,26 +62,25 @@ public class UserMessageManager {
     }
 
     /**
-     * WORK IN PROGRESS!!!
      * Allows a user to respond to a TradeRequestMessage
      * @param username the username of the user responding (PersonB) to this message
      * @param message the TradeRequestMessage object
      */
     public void manageTradeRequest(String username, TradeRequestMessage message) {
-        // read in Scanner inputs, set up tradeRequestManager
+        // read in Scanner inputs
         Scanner input = new Scanner(System.in);
         TradeRequest tradeRequest = message.getTradeContent();
         String exitInput = "";
         while(!exitInput.equals("back")) {
-            System.out.println(message);
+            System.out.println(message.toString());
             System.out.println("Enter 'back' to go back to the messages menu.");
             exitInput = input.nextLine();
             // confirm
             if (exitInput.toLowerCase().equals("confirm")) {
-                exitInput = "back";
-                System.out.println("Trade confirmed.");
                 // create a trade object from the TradeRequest
                 // confirmTrade(trade whatever);
+                System.out.println("Trade confirmed.");
+                exitInput = "back";
             }
             // deny
             else if (exitInput.toLowerCase().equals("deny")) {
@@ -92,9 +93,22 @@ public class UserMessageManager {
             }
             // edit
             else if (exitInput.toLowerCase().equals("edit")) {
-                // if(
-                    // editTradeRequest(message.getTradeContent());
-                // else {System.out.println("Sorry, you have run out of the maximum number of edits. Confirm or deny this trade."}
+                TradeRequestManager tradeRequestManager = new TradeRequestManager(tradeRequest);
+                if(tradeRequestManager.canEdit(username, tradeRequest)) {
+                    try {
+                        TradeRequestMessage requestMessage = editTradeRequest(username,
+                                message.getTradeContent(), tradeRequestManager);
+                        String tradePartner = tradeRequest.getTradePartner(username);
+                        // this.allUsers.get(username).removeMessage(message);
+                        // this.allUsers.get(tradePartner).addMessage(requestMessage);
+                        System.out.println("Edit to this trade request successful! Returning from menu.");
+                        exitInput = "back";
+                    }
+                    catch(ParseException ex) {
+                        System.out.println("You input date information incorrectly! Try again.");
+                    }
+                }
+                else System.out.println("You have reached the maximum number of edits. Confirm or deny this trade.");
             }
             // if none of the above and not "back"
             else if(!exitInput.toLowerCase().equals("back")) {
@@ -103,16 +117,50 @@ public class UserMessageManager {
         }
     }
 
-    /*
-    private TradeRequestMessage editTradeRequest(TradeRequest tradeRequest) {
-        // System.out.println("Current time of trade: " + get the time of trade + "\n" +
-        // "Enter in a new time of trade.");
-        // figure out how to format user input into java date object
-        // System.out.println("Current location of trade: " + get location of trade + "\n" +
-        // "Enter in a new location to meet at.");
-        // String newLocation = input.nextLine();
-        // set new location/date in traderequest, change the # of edits, put into (old/new) trade request message
-        // send to other use with "edits made" notification somewhere
-    }
+    /**
+     * Private helper for manageTradeRequest that edits a trade request.
+     * @param username name of user editing this trade request
+     * @param tradeRequest the trade request object
+     * @return The now edited trade request object in a TradeRequestMessage
+     * @throws ParseException if date is input wrong (might be finicky!)
      */
+    private TradeRequestMessage editTradeRequest(String username, TradeRequest tradeRequest,
+                                                 TradeRequestManager tradeRequestManager) throws ParseException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Edit time, location, or both?");
+        String choice = input.nextLine();
+        // edit time
+        if(choice.toLowerCase().equals("time")) {
+            System.out.println("Old time + " + tradeRequest.getDate() + "\n" +
+                    "Enter in new time to meet up in 'DD/MM/YYYY format.");
+            String newDateInput = input.nextLine();
+            Date newDate = new SimpleDateFormat("dd/MM/yyyy").parse(newDateInput);
+            tradeRequestManager.setDate(username, newDate);
+            return new TradeRequestMessage("User " + username + " has made an edit to your trade request.",
+                    tradeRequestManager.getTradeRequest());
+        }
+        // edit location
+        else if(choice.toLowerCase().equals("location")) {
+            System.out.println("Old location: " + tradeRequest.getPlace() + "\n" +
+                    "Enter in new location to meet up. ");
+            String newPlace = input.nextLine();
+            tradeRequestManager.setPlace(username, newPlace);
+            return new TradeRequestMessage("User " + username + " has made an edit to your trade request.",
+                    tradeRequestManager.getTradeRequest());
+        }
+        // edit both
+        else {
+            System.out.println("Old time + " + tradeRequest.getDate() + "\n" +
+                    "Enter in new time to meet up in 'DD/MM/YYYY format.");
+            String newDateInput = input.nextLine();
+            Date newDate = new SimpleDateFormat("dd/MM/yyyy").parse(newDateInput);
+            tradeRequestManager.setDate(username, newDate);
+            System.out.println("Old location: " + tradeRequest.getPlace() + "\n" +
+                    "Enter in new location to meet up. ");
+            String newPlace = input.nextLine();
+            tradeRequestManager.setPlace(username, newPlace);
+            return new TradeRequestMessage("User " + username + " has made an edit to your trade request.",
+                    tradeRequestManager.getTradeRequest());
+        }
+    }
 }
