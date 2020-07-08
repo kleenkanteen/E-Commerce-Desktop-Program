238926d3c5078685e92c5c1364a21e415843bd3f
Abenav <abenav.vijayakumaran@mail.utoronto.ca>
2020-07-08 06:32:36 -0400

    - removed AdminAccountManagement.java, AdminFreezeRequestManager.java, AdminMenuOptions.java, AdminMessageManager.java, AdminNewMessageManager.java
    - replaced Main.java
    - MainMenu.java interacts with the created Gateways, saves files appropriately
    - made GlobalInventoryGateways methods public.

---
+++ phase1/src/controller_presenter_gateway/Main.java
@@ -0,0 +1,11 @@
+package controller_presenter_gateway;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        MainMenu menu = new MainMenu();
+        menu.run();
+    }
+}
---
+++ phase1/src/controller_presenter_gateway/MainMenu.java
@@ -0,0 +1,71 @@
+package controller_presenter_gateway;
+
+import entities.Admin;
+import entities.Item;
+import entities.User;
+import exceptions.InvalidLoginException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class MainMenu {
+    public void run() throws IOException {
+        System.out.println("run!");
+
+        String serializedAdmins = "src/managers/serializedAdmins.ser";
+        String serializedUsers = "src/managers/serializedUsers.ser";
+        String serializedGlobalInventory = "src/managers/serializedGlobalInventory.ser";
+        String serializedItemIDs = "src/managers/serializedItemIDs.ser";
+        //String serializedMessages = "src/managers/serializedMessages.ser";
+
+        UserGateway ug = new UserGateway(serializedUsers);
+        HashMap<String, User> userHashMap = ug.getMapOfUsers();
+
+        AdminAccountGateways ag = new AdminAccountGateways(serializedAdmins);
+        HashMap<String, Admin> adminHashMap = ag.getAdminMap();
+
+        GlobalInventoryGateways gig = new GlobalInventoryGateways(serializedGlobalInventory);
+        HashMap<String, Item> itemHashMap = gig.gI.getItemMap();
+
+        //TODO: add gateway to deserialize itemIDs so we can also pass that.
+        //TODO: message deserialization? or is all of that stored in each admin/ user acc?
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = "";
+        while (!input.equals("exit")) {
+            System.out.println("Type:\n'1' for User Login.\n'2' for User Account Creation.\n'3' for Admin Login.\n'exit' to exit the program.");
+            try {
+                input = br.readLine();
+                if (input.equals("1") || input.equals("2") || input.equals("3")) {
+                    System.out.println("Enter username:");
+                    String username = br.readLine();
+                    System.out.println("Enter password");
+                    String pass = br.readLine();
+                    if (input.equals("1") || input.equals("2")){
+                        UserLogin ul = new UserLogin(username, pass);
+                        if (input.equals("1")){
+                            if ((ul.login(userHashMap)).equals(username)) {
+                                UserMenu um = new UserMenu(username, userHashMap);
+                            }
+                        }
+                        else {
+                            System.out.println("avoiding error");
+                            //TODO: bring up concerns w this
+                        }
+                    }
+                    else {
+                        AdminLogin y = new AdminLogin(username, pass, adminHashMap);
+                    }
+                }
+            } catch (IOException | InvalidLoginException | ClassNotFoundException e) {
+                System.out.println("Something went wrong");
+            }
+            ug.writeToFile(serializedUsers, userHashMap);
+            ag.saveToFile(adminHashMap);
+            // gig.writeToFile(itemHashMap);
+        }
+    }
+}
+
