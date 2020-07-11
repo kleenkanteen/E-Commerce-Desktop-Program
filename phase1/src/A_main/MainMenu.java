db0c2af92f1468ff0d1fbbe0a843cb38429b0fe1
Abenav <abenav.vijayakumaran@mail.utoronto.ca>
2020-07-11 13:31:26 -0400

    - mainmenu should work with all user functionality (adding user + methods provided once correct login supplied) - nothing is sent to admin. adding admin functionality right now

--- phase1/src/A_main/MainMenu.java
+++ phase1/src/A_main/MainMenu.java
@@ -2,6 +2,7 @@ package A_main;
 
 import C_controllers.AdminSystem;
 import C_controllers.UserMenu;
+import E_use_cases.UserManager;
 import F_entities.*;
 import G_exceptions.InvalidLoginException;
 import G_exceptions.InvalidUsernameException;
@@ -11,84 +12,113 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 
 public class MainMenu {
     public void run() throws IOException, ClassNotFoundException {
-
-        String serializedAdmins = "phase1/src/ser_file_infos/serializedAdmins.ser";
-        String serializedUsers = "phase1/src/ser_file_infos/serializedUsers.ser";
-        String serializedGlobalInventory = "phase1/src/ser_file_infos/serializedGlobalInventory.ser";
-        String serializedAdminMessages = "phase1/src/ser_file_infos/serializedAdminMessages.ser";
-        String serializedGlobalWishlist = "phase1/src/ser_file_infos/serializedGlobalWishlist.ser";
-        String serializedUserTrades = "phase1/src/ser_file_infos/serializedUserTrades.ser";
-
-        String dirName = "phase1/src/ser_file_infos";
-
-        Files.list(new File(dirName).toPath())
-                .limit(10)
-                .forEach(path -> {
-                    System.out.println(path);
-                });
-
+        String serializedAdmins = "phase1/src/H_ser_file_infos/serializedAdmins.ser";
+        String serializedUsers = "phase1/src/H_ser_file_infos/serializedUsers.ser";
+        String serializedGlobalInventory = "phase1/src/H_ser_file_infos/serializedGlobalInventory.ser";
+        String serializedAdminMessages = "phase1/src/H_ser_file_infos/serializedAdminMessages.ser";
+        String serializedGlobalWishlist = "phase1/src/H_ser_file_infos/serializedGlobalWishlist.ser";
+        String serializedUserTrades = "phase1/src/H_ser_file_infos/serializedUserTrades.ser";
+        //deserialize users
         UserGateway ug = new UserGateway(serializedUsers);
-        HashMap<String, User> userHashMap = ug.getMapOfUsers();
+        System.out.println("Users:\n" + ug.getMapOfUsers());
+        /*Item temporaryItem = new Item("bike", "abenav", "old rusty bike");
+        int rand = getRandom(40000);
+        String randString = Integer.toString(rand);
+        temporaryItem.setItemID(randString);
+         */
+        //deserialize global inventory
+        GlobalInventoryGateways gig = new GlobalInventoryGateways(serializedGlobalInventory);
+        System.out.println("Global inventory:\n" + gig.getgI());
+        /*GlobalInventory tempinv = new GlobalInventory();
+        ArrayList<String> itemIdCollection = new ArrayList<>();
+        tempinv.getItemMap().put(randString, temporaryItem);
+        tempinv.getItemIdCollection().add(randString);
+        gig.writeToFile(tempinv);
 
+        Item temporaryItem2 = new Item("car", "abenav", "honda accord");
+        rand = getRandom(40000);
+        randString = Integer.toString(rand);
+        temporaryItem2.setItemID(randString);
+        gig.getgI().addItem(temporaryItem2.getItemID(), temporaryItem2);
+        tempinv = gig.getgI();
+         */
         /*AdminAccountGateways ag = new AdminAccountGateways(serializedAdmins);
         HashMap<String, Admin> adminHashMap = ag.getAdminMap();*/
-        Admin primeAdmin = new Admin("a", "a");
-        AdminAccountGateways ag = new AdminAccountGateways(serializedAdmins);
-        HashMap<String, Admin> adminHashMap = ag.getAdminMap();
-        if (!adminHashMap.containsKey("a")){
-            adminHashMap.put("a", primeAdmin);
-        }
-        ag.saveToFile(adminHashMap);
+        /* User newuser = new User("abenav", "abenav");
+        User newuser2 = new User("pylon", "pylon");
+        HashMap<String, User> allUsers = new HashMap<>();
+        allUsers.put(newuser.getUsername(), newuser);
+        allUsers.put(newuser2.getUsername(), newuser2);
+        Item speakers = new Item("speakers", newuser.getUsername(), "micca mb42");
+        ArrayList<Item> user1trades = new ArrayList<>();
+        user1trades.add(speakers);
+        Item phone = new Item("phone", newuser2.getUsername(), "samsung s9");
+        ArrayList<Item> user2trades = new ArrayList<>();
+        user2trades.add(phone);
+        LocalDateTime rightNow1 = LocalDateTime.now();
+        LocalDateTime endDate = LocalDateTime.of(2021, Month.JUNE, 2, 2, 34, 2);
+        TempTrade newtrade = new TempTrade(newuser.getUsername(), newuser2.getUsername(), user1trades, user2trades, rightNow1, endDate);
+        ArrayList<Trade> tempArray = new ArrayList<>();
+        tempArray.add(newtrade);
+        HashMap<String, ArrayList<Trade>> userTradez = new HashMap<>();
+        userTradez.put(newuser.getUsername(), tempArray);
 
-        AdminMessageGateway amg = new AdminMessageGateway(serializedAdminMessages);
-        ArrayList<Message> adminMessagesArrayList = amg.getMessages();
-
-        GlobalInventoryGateways gig = new GlobalInventoryGateways(serializedGlobalInventory);
-        GlobalInventory globalInv = gig.getgI();
-
-        GlobalWishlistGateway gwl = new GlobalWishlistGateway(serializedGlobalWishlist);
-        GlobalWishlist globalWList = gwl.getWishlistItems();
-
+        /*
+        System.out.println("User trade info:\n" + userTradez);
         UserTradesGateway utg = new UserTradesGateway(serializedUserTrades);
-        HashMap<String, ArrayList<Trade>> userTrades = utg.getUserTrades();
-
+        utg.writeToFile(serializedUserTrades, userTradez);*/
+        //deserialize all user trades
+        UserTradesGateway utg = new UserTradesGateway(serializedUserTrades);
+        System.out.println("User trade info:\n" +utg.getUserTrades());
+        /* HashMap<String, Item> itemMap1 = new HashMap<String, Item>();
+        ArrayList<String> itemIDcollection1 = new ArrayList<>();
+        int p = getRandom(5000);
+        String p2 = Integer.toString(p);
+        itemIDcollection1.add(p2);
+        System.out.println(p2);
+        Item noo = new Item("asd", "afa", "old sfas");
+        noo.setItemID(p2);
+        itemMap1.put(p2, noo);
+        GlobalInventory tempGlobal = new GlobalInventory();
+        tempGlobal.setItemMap(itemMap1);
+        tempGlobal.addItemIdToCollection(p2);
+        System.out.println("Global inventory:\n" + gig.getgI());
+        */
+        /* GlobalWishlist globo = new GlobalWishlist();
+        globo.addWish(p2, newuser.getUsername());
+        System.out.println("Messages: \n"+globo);
+        */
+        //deserialize GlobalWishlistGateway
+        GlobalWishlistGateway gwl = new GlobalWishlistGateway(serializedGlobalWishlist);
+        System.out.println("Global Wishlist Items:\n" + gwl.getWishlistItems());
+        /*ArrayList <Message> tempray = new ArrayList<>();
+        FreezeRequestMessage xd = new FreezeRequestMessage("im freezing you!", newuser.getUsername());
+        tempray.add(xd);
+        System.out.println("Messages: \n"+tempray);
+         */
+        //deserialize AdminMessageGateway
+        AdminMessageGateway amg = new AdminMessageGateway(serializedAdminMessages);
+        System.out.println("Admin Messages:\n"+ amg.getMessages());
+        /*amg.writeToFile(serializedAdminMessages, tempray);
+         */
+        /*
+        UserMenu um = new UserMenu("abenav", ug.getMapOfUsers(), utg.getUserTrades(), gig.getgI(), gwl.getWishlistItems(), amg.getMessages());
+        um.run();
+        ug.writeToFile(serializedUsers, ug.getMapOfUsers());
+        gig.writeToFile(gig.getgI());
+        utg.writeToFile(serializedUserTrades, utg.getUserTrades());
+        gwl.writeToFile(serializedGlobalWishlist,  gwl.getWishlistItems());
+        amg.writeToFile(serializedAdminMessages, amg.getMessages());
+         */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-/*
-        Admin x = new Admin("ball", "bowl");
-        HashMap<String, Admin> adminList = new HashMap<>();
-        adminList.put(x.getUsername(), x);
-
-        User x2 = new User("hi", "hello");
-        HashMap<String, User> allUser = new HashMap<>();
-        allUser.put(x2.getUsername(), x2);
-
-        FreezeRequestMessage x3 = new FreezeRequestMessage("blah", x.getUsername());
-        ArrayList<Message> adminMessageList = new ArrayList<>();
-        adminMessageList.add(x3);
-
-        HashMap<String, Item> itemMap = new HashMap<String, Item>();
-        Item tempItem = new Item("duck", "hi", "this is a dubber ruck");
-        int d = getRandom(40);
-        String str = String.valueOf(d);
-        System.out.println(str);
-        itemMap.put(str, tempItem);
-        ArrayList<String> itemIdCollection = new ArrayList<>();
-        itemIdCollection.add(str);
-        GlobalInventory x4 = new GlobalInventory();
-        x4.addItem(str, tempItem);
-
-        GlobalWishlist globalwl = new GlobalWishlist();
-        globalwl.addWish(str, x2.getUsername());
-
-      new AdminMenu(x);*/
-
         String input = "";
         System.out.println("Type:\n'1' for User Login.\n'2' for User Account Creation.\n'3' for Admin Login.\nAny other value to exit the program.");
         try {
@@ -99,46 +129,32 @@ public class MainMenu {
                 System.out.println("Enter password");
                 String pass = br.readLine();
                 if (input.equals("1") || input.equals("2")){
-                    UserLogin ul = new UserLogin(username, pass);
-                    if (input.equals("1") && (ul.login(userHashMap)).equals(username)){
-                        UserMenu um = new UserMenu(username, userHashMap, userTrades, globalInv, globalWList, adminMessagesArrayList);
-                        um.run();
+                    UserManager attempt = new UserManager(ug.getMapOfUsers());
+                    attempt.login(username, pass);
+                    if (input.equals("1")){
+                        if (attempt.login(username, pass)) {
+                            UserMenu um = new UserMenu(username, ug.getMapOfUsers(), utg.getUserTrades(), gig.getgI(), gwl.getWishlistItems(), amg.getMessages());
+                            um.run();
+                        }
                     }
-                    else {
-                        ul.createNewUser(userHashMap);
+                    else
+                        attempt.createNewUser(username, pass);
                     }
-                }
                 else {
-                    AdminLogin y = new AdminLogin(username, pass, adminHashMap);
-                    if (y.login().equals(username)) {
-                        System.out.println("Valid login.");
-                        Admin loggedInAdmin = y.getAdminObject();
-                        System.out.println("2");
-
-                        AdminSystem am = new AdminSystem(loggedInAdmin, adminHashMap,
-                                adminMessagesArrayList, userHashMap, globalInv);
-                        System.out.println("3");
-
+                    System.out.println("TODO");
                     }
                 }
-            }
             else {
                 System.out.println("Exiting program.");
             }
-        } catch (IOException | InvalidLoginException | InvalidUsernameException e) {
+        } catch (IOException e) {
             System.out.println("Something went wrong");
         }
-        ug.writeToFile(serializedUsers, userHashMap);
-        ag.saveToFile(adminHashMap);
-        amg.writeToFile(serializedAdminMessages, adminMessagesArrayList);
-        gig.writeToFile(globalInv);
-        gwl.writeToFile(serializedGlobalWishlist, globalWList);
-        utg.writeToFile(serializedUserTrades, userTrades);
-
-    }
-    //TODO: remove this
-    public static int getRandom(int max){
-        return (int) (Math.random()*max);
+        ug.writeToFile(serializedUsers, ug.getMapOfUsers());
+        gig.writeToFile(gig.getgI());
+        utg.writeToFile(serializedUserTrades, utg.getUserTrades());
+        gwl.writeToFile(serializedGlobalWishlist,  gwl.getWishlistItems());
+        amg.writeToFile(serializedAdminMessages, amg.getMessages());
     }
 }
 
