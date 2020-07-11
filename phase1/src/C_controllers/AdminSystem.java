af9d70a8a0bb8499e69aaa5d00eaea07f6eca80b
QIyi Zhang <qiyi.zhang@mai.utoronto.ca>
2020-07-11 18:14:04 -0400

    change the parameter

--- phase1/src/C_controllers/AdminSystem.java
+++ phase1/src/C_controllers/AdminSystem.java
@@ -1,21 +1,16 @@
 package C_controllers;
 
 import D_presenters.AdminMenu;
-
 import F_entities.Admin;
-import F_entities.GlobalInventory;
 import F_entities.Message;
-import F_entities.User;
 import E_use_cases.AdminManager;
 import E_use_cases.GlobalInventoryManager;
 import E_use_cases.UserManager;
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
+
 
 public class AdminSystem {
     Admin admin;
@@ -46,7 +41,8 @@ public class AdminSystem {
         am.printMainOption();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input;
-        while (true) {
+        boolean t = true;
+        while (t) {
             try {
                 input = br.readLine();
                 switch (input) {
@@ -55,21 +51,20 @@ public class AdminSystem {
                         AdminMessageReplySystem amr = new AdminMessageReplySystem(adminManager, gim,
                                 um, admin.getUsername());
                         amr.run();
-                        break;
+
                     case "2":
                         AdminAccountSystem aas = new AdminAccountSystem(admin, adminManager, adminMessageList);
                         aas.run();
 
-                        break;
                     case "3":
                         AdminBrowsingUsers abu = new AdminBrowsingUsers(um);
                         abu.start();
 
-                        break;
+
                     case "4":
+                        t = false;
                         am.exitPresenter();
 
-                        break;
                 }
 
             } catch (IOException e) {
