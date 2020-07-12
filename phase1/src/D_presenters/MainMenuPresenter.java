0011b9011d309ac1df68ee495dc8a299c9582f60
Sabih <sabih.sarowar@mail.utoronto.ca>
2020-07-12 12:42:37 -0400

    small english

--- phase1/src/D_presenters/MainMenuPresenter.java
+++ phase1/src/D_presenters/MainMenuPresenter.java
@@ -2,17 +2,17 @@ package D_presenters;
 
 public class MainMenuPresenter {
     public void printExit(){
-        System.out.println("exiting you from the program");
+        System.out.println("Exiting you from the program");
     }
     public void printMenuPrompt(){
-        System.out.println("Choose your option below:\n[1] for User Login.\n[2] for User Account Creation.\n" +
-                "[3] for Admin Login.\nAny other value to exit the program.");
+        System.out.println("Choose your option below:\n[1] User Login.\n[2] User Account Creation.\n" +
+                "[3] Admin Login.\nAny other value to exit the program.");
     }
     public void printLoginPrompt1(){
-        System.out.println("Enter username:");
+        System.out.println("Enter username:\n");
     }
     public void printLoginPrompt2(){
-        System.out.println("Enter password");
+        System.out.println("Enter password:\n");
     }
     public void inputError(){
         System.out.println("An error has occurred when you input");
