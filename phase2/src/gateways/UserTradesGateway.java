2b4a05afb3b119f086f7a3ec17f10cbf613c10a8
Jing Jing Yuan <angel.yuan@mail.utoronto.ca>
2020-07-24 17:15:47 -0400

    change the gateway abstraction

--- phase2/src/gateways/UserTradesGateway.java
+++ phase2/src/gateways/UserTradesGateway.java
@@ -5,10 +5,12 @@ import entities.Trade;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class UserTradesGateway {
 
-    HashMap<String, ArrayList<Trade>> userTrades;
+    Map<String, List<Trade>> userTrades;
 
     /**
      * Creates a new gateway that loads in the HashMap of users and their respective trades for an .ser file.
@@ -35,15 +37,15 @@ public class UserTradesGateway {
      * @throws IOException If the file cannot be read
      * @throws ClassNotFoundException If the class cannot be found
      */
-    public HashMap<String, ArrayList<Trade>> readFromFile(String filepath) throws IOException, ClassNotFoundException{
-        HashMap<String, ArrayList<Trade>> userTrades2 = new HashMap<>();
+    public Map<String, List<Trade>> readFromFile(String filepath) throws IOException, ClassNotFoundException{
+        Map<String, List<Trade>> userTrades2 = new HashMap<>();
 
         // load in the objects
         InputStream file = new FileInputStream(filepath);InputStream buffer = new BufferedInputStream(file);
         ObjectInput input = new ObjectInputStream(buffer);
 
         // deserialize the hashmap of user objects
-        userTrades2 = (HashMap<String, ArrayList<Trade>>) input.readObject();
+        userTrades2 = (Map<String, List<Trade>>) input.readObject();
         input.close();
         return userTrades2;
     }
@@ -54,7 +56,7 @@ public class UserTradesGateway {
      * @param userTrades3 HashMap of the trades for all users.
      * @throws IOException when an error occur when serializing
      */
-    public void writeToFile(String filepath, HashMap<String, ArrayList<Trade>> userTrades3) throws IOException{
+    public void writeToFile(String filepath, Map<String, List<Trade>> userTrades3) throws IOException{
         // load allUsers onto the file at designed path
         FileOutputStream file = new FileOutputStream(filepath);
         OutputStream buffer = new BufferedOutputStream(file);
@@ -70,7 +72,7 @@ public class UserTradesGateway {
      * returns the HashMap of trades for all users.
      * @return HashMap of trades for all users that is presently serialized.
      */
-    public HashMap<String, ArrayList<Trade>> getUserTrades() {
+    public Map<String, List<Trade>> getUserTrades() {
         return userTrades;
     }
 }
