39594564581a868486c496507fa6292ed18970c4
Oliver Li <oliverli.li@mail.utoronto.ca>
2020-07-26 06:28:45 -0400

    [1] UserMenu partially broken down into subclasses BrowseThroughX [2] BannedUser functionality finished [3] MessageBuilder implemented into UserMenu [4] Javadocs added into MessageBuilder, getUnbanRequest added

--- phase2/src/presenters/BannedUserPresenter.java
+++ phase2/src/presenters/BannedUserPresenter.java
@@ -2,11 +2,13 @@ package presenters;
 
 public class BannedUserPresenter {
     public void unBanRequestPrompt() {
-        System.out.println("Would you like to submit an unban request to the administrators? \n[1] Yes. \n[2] No.");
+        System.out.println("Would you like to beg for forgiveness to our glorious overlords? " +
+                "\n[1] Plead for mercy. " +
+                "\n[2] Remain a heretic.");
     }
 
     public void unbanRequestSent() {
-        System.out.println("Unban request sent.");
+        System.out.println("Congratulations! You have begged for forgiveness! :)");
     }
 
     public void returningToMainMenu() {
