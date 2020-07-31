d9e9ad4557524961ce6cbf8db920d2924231752b
Haoluan Chen <haoluan.chen@mail.utoronto.ca>
2020-07-30 23:45:46 -0400

    rename

--- phase2/src/frontend/GlobalInventoryMenuMain.java
+++ phase2/src/frontend/GlobalInventoryMenuMain.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class GlobalInventoryMenuListener extends Application {
+public class GlobalInventoryMenuMain extends Application {
 
     public static void main(String[] args) {
         launch(args);
