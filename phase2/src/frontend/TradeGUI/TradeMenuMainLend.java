1c526628794a735aa511867ddc1c66586f058c05
programmerpc <thanusun.thayaparan@mail.utoronto.ca>
2020-08-07 17:13:31 -0400

    Adding TradeMenuLend.fxml, TradeMenuMainLend, TradeMenuMainLendController Also, in order to call this controller, you MUST call and provide all the inputs in buildController, otherwise you will get a NullPointerException.

---
+++ phase2/src/frontend/TradeGUI/TradeMenuMainLend.java
@@ -0,0 +1,29 @@
+package frontend.TradeGUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class TradeMenuMainLend extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loadTradeMenu = new FXMLLoader(getClass().getResource("TradeMenuLend.fxml"));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        loadTradeMenu.setController(new TradeMenuMainLendController());
+        Parent root = loadTradeMenu.load();
+        primaryStage.setTitle("Lend Trade");
+        Scene tradeScene = new Scene(root, 600, 400);
+        primaryStage.setScene(tradeScene);
+        primaryStage.show();
+    }
+}
